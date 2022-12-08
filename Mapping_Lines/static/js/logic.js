56dc6b9449cc9545c78cbe19d96e57846bfcb46a
Owen-Wang1234 <owen.wang.89@gmail.com>
2022-12-07 23:05:22 -0600

    New branch for mapping lines created

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,33 @@
+// Add console.log to check to see if the code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map("mapid").setView([40.7, -94.5], 4);
+
+// Create the tile layer that will be the background of the map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/dark-v10",
+    accessToken: API_KEY
+});
+
+// Then add the "graymap" tile layer to the map.
+streets.addTo(map);
+
+// Get data from cities.js.
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: "orange",
+        weight: 4
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population: " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});

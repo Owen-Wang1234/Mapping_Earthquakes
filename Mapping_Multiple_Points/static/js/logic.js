d5512cc37c3b177ee26471357cae25dfffeab20a
Owen-Wang1234 <owen.wang.89@gmail.com>
2022-12-07 22:56:28 -0600

    Population-sized markers placed in US

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid").setView([34.0522, -118.2437], 14);
+let map = L.map("mapid").setView([40.7, -94.5], 4);
 
 // Create the tile layer that will be the background of the map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -17,9 +17,17 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 // Then add the "graymap" tile layer to the map.
 streets.addTo(map);
 
-// Add a circle marker to the map for Los Angeles, California.
-let marker = L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: "#ffffa1",
-}).addTo(map);
+// Get data from cities.js.
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: "orange",
+        weight: 4
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population: " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
